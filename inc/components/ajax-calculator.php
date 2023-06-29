b3f5c6ea97c2ee9df4f2b5b148a254eab2cef016
nikki35944 <95740936+nikki35944@users.noreply.github.com>
2023-06-29 22:10:34 +0300

    ajax calculator validator

--- inc/components/ajax-calculator.php
+++ inc/components/ajax-calculator.php
@@ -1,6 +1,8 @@
 <?php
 
 if ( !empty($_POST['first_number']) && !empty($_POST['operations']) && !empty($_POST['second_number']) ) {
+    if (filter_var($_POST['first_number'], FILTER_VALIDATE_INT) || filter_var($_POST['operations'], FILTER_VALIDATE_INT)) echo 'enter numbers';
+} else {
     $firstNumber = $_POST['first_number'];
     $operation = $_POST['operations'];
     $secondNumber = $_POST['second_number'];
